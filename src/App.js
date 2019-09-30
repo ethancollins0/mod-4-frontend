3416404274f24b85c523da049bd85aa38cf498d8
Ethan Collins <ethantcollins98@gmail.com>
2019-09-30 14:55:42 -0600

    heroku

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 export default class App extends Component {
 
   componentDidMount(){
-    fetch('http://localhost:3001/')
+    fetch('https://property-manager-backend.herokuapp.com/home')
       .then(resp => resp.json())
       .then(console.log)
   }
