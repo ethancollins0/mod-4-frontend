3b5c3f77c44b7bf2647a1874a932d79760137225
Ethan Collins <ethantcollins98@gmail.com>
2019-10-04 11:08:11 -0600

    working employee delete

--- src/components/Employees/EmployeeContainer.js
+++ src/components/Employees/EmployeeContainer.js
@@ -2,11 +2,11 @@ import React from 'react'
 import Employee from './Employee'
 import './Employee.css'
 
-const EmployeeContainer = ({ employees, select }) => {
+const EmployeeContainer = ({ employees, select, deleteEmployee }) => {
 
     const renderEmployees = () => {
         return employees.map(employee => (
-            <Employee select={select} employee={employee} />
+            <Employee deleteEmployee={deleteEmployee} select={select} employee={employee} />
         ))
     }
 
@@ -15,6 +15,7 @@ const EmployeeContainer = ({ employees, select }) => {
             <div className='employee-row'>
                 <span className='title'><strong>Employee</strong></span>
                 <span className='title'><strong>Email</strong></span>
+                <span className='title'><strong>Delete</strong></span>
             </div>
             {renderEmployees()}
         </div>
